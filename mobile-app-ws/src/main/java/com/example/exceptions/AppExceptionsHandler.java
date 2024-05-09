6f24355088756265dd2349ca7aaf3f42799e9614
Yuthika9 <167095094+Yuthika9@users.noreply.github.com>
2024-05-09 13:58:14 +0530

    Add custom exceptions

--- mobile-app-ws/src/main/java/com/example/exceptions/AppExceptionsHandler.java
+++ mobile-app-ws/src/main/java/com/example/exceptions/AppExceptionsHandler.java
@@ -14,9 +14,41 @@ import java.util.Date;
 @ControllerAdvice
 public class AppExceptionsHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(value = {Exception.class})
 
+    @ExceptionHandler(value = {Exception.class})
     public ResponseEntity<Object> handleAnyException(Exception ex, WebRequest request) {
+
+        String errorMessageDescription = ex.getLocalizedMessage();
+
+        if (errorMessageDescription == null) {
+            errorMessageDescription = ex.getClass().getSimpleName();
+        }
+
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), errorMessageDescription);
+
+        return new ResponseEntity<>(
+                errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+    @ExceptionHandler(value = {NullPointerException.class})
+    public ResponseEntity<Object> handleNullPointerException(NullPointerException ex, WebRequest request) {
+
+        String errorMessageDescription = ex.getLocalizedMessage();
+
+        if (errorMessageDescription == null) {
+            errorMessageDescription = ex.getClass().getSimpleName();
+        }
+
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), errorMessageDescription);
+
+        return new ResponseEntity<>(
+                errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+
+    @ExceptionHandler(value = {UserServiceException.class})
+    public ResponseEntity<Object> handleUserServiceException(UserServiceException ex, WebRequest request) {
 
         String errorMessageDescription = ex.getLocalizedMessage();
 
