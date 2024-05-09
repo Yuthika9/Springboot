44e72850ed466e29afcbce9efa6b4014e47eb5df
Yuthika9 <167095094+Yuthika9@users.noreply.github.com>
2024-05-09 13:51:13 +0530

    Add general exceptions

--- mobile-app-ws/src/main/java/com/example/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/example/ui/controller/UserController.java
@@ -36,6 +36,9 @@ public class UserController{
 					})
 	public ResponseEntity<UserRest> getUser(@PathVariable String userId)
 	{
+		String firstName = null;
+		int firstNameLength = firstName.length();
+
 		if(users.containsKey(userId))
 			return new ResponseEntity<>(users.get(userId), HttpStatus.OK);
 		else
