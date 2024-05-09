ad5225787eb9a3d9a517e1384ddb8d6499a5b09e
Yuthika9 <167095094+Yuthika9@users.noreply.github.com>
2024-05-09 10:48:00 +0530

    Returning a Response

--- mobile-app-ws/src/main/java/com/example/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/example/ui/controller/UserController.java
@@ -1,20 +1,17 @@
 package com.example.ui.controller;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.UserRest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("users") //http://localhost:8080/users
 
 public class UserController{
-	
-	
+
 	@GetMapping()
-	public String getUser(@RequestParam (value = "page", defaultValue = "1") int page,
+	public String getUser(@RequestParam(value = "page", defaultValue = "1") int page,
 						  @RequestParam (value = "limit",defaultValue = "20") int size,
 						  @RequestParam (value = "sort" , required = false, defaultValue = "desc") String sort)
 
@@ -22,10 +19,16 @@ public class UserController{
 		return "get user was called with page " +page + " ,limit " +size + " ,sort " +sort;
 	}
 
-	@GetMapping(path = "/users/{userId}")
-	public String getUser(@PathVariable String userId)
+	@GetMapping(path = "{userId}")
+	public ResponseEntity<UserRest> getUser(@PathVariable String userId)
 	{
-		return "get user was called with userId: " + userId;
+		UserRest returnValue = new UserRest();
+
+		returnValue.setFirstName("Yuth");
+		returnValue.setLastName("Huang");
+		returnValue.setEmail("yuthhuanghuang@gmail.com");
+
+		return new ResponseEntity<>(returnValue, HttpStatus.BAD_REQUEST);
 	}
 	
 	@PostMapping
